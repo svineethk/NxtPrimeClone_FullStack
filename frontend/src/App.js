6cdd470b3eb4c1cdd1ffdf4a8646d15d79127890
svineethk <vineethkumar.selvam@gmail.com>
2025-04-27 12:09:02 +0530

    Frontend Initiated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
+import {Route, Routes, Navigate} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  decrementCartItemQuantity = id => {
+    const {cartList} = this.state
+    const productObject = cartList.find(eachCartItem => eachCartItem.id === id)
+    if (productObject.quantity > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (id === eachCartItem.id) {
+            const updatedQuantity = eachCartItem.quantity - 1
+            return {...eachCartItem, quantity: updatedQuantity}
+          }
+          return eachCartItem
+        }),
+      }))
+    }
+  }
+
+  incrementCartItemQuantity = id => {
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(eachCartItem => {
+        if (id === eachCartItem.id) {
+          const updatedQuantity = eachCartItem.quantity + 1
+          return {...eachCartItem, quantity: updatedQuantity}
+        }
+        return eachCartItem
+      }),
+    }))
+  }
+
+  removeAllCartItems = () => {
+    this.setState({cartList: []})
+  }
+
+  removeCartItem = id => {
+    const {cartList} = this.state
+    const updatedCartList = cartList.filter(
+      eachCartItem => eachCartItem.id !== id,
+    )
+    this.setState({cartList: updatedCartList})
+  }
+
+  addCartItem = product => {
+    this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+          removeAllCartItems: this.removeAllCartItems,
+          incrementCartItemQuantity: this.incrementCartItemQuantity,
+          decrementCartItemQuantity: this.decrementCartItemQuantity,
+        }}
+      >
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+          <Route path="/products/:id" element={<ProtectedRoute><ProductItemDetails /></ProtectedRoute>} />
+          <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
+        </Routes>
+      </CartContext.Provider>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/LoginForm/index.js
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+import { useNavigate, Navigate } from 'react-router-dom' // <-- Import Navigate here
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [showSubmitError, setShowSubmitError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const navigate = useNavigate()
+
+  const onChangeUsername = event => {
+    setUsername(event.target.value)
+  }
+
+  const onChangePassword = event => {
+    setPassword(event.target.value)
+  }
+
+  const onSubmitSuccess = jwtToken => {
+    Cookies.set('jwt_token', jwtToken, {
+      expires: 30,
+    })
+    navigate('/')
+  }
+
+  const onSubmitFailure = errorMsg => {
+    setShowSubmitError(true)
+    setErrorMsg(errorMsg)
+  }
+
+  const submitForm = async event => {
+    event.preventDefault()
+    const userDetails = { username, password }
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      onSubmitSuccess(data.jwt_token)
+    } else {
+      onSubmitFailure(data.error_msg)
+    }
+  }
+
+  const renderPasswordField = () => {
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-field"
+          value={password}
+          onChange={onChangePassword}
+          placeholder="Password"
+        />
+      </>
+    )
+  }
+
+  const renderUsernameField = () => {
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-field"
+          value={username}
+          onChange={onChangeUsername}
+          placeholder="Username"
+        />
+      </>
+    )
+  }
+
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken !== undefined) {
+    return <Navigate to="/" />
+  }
+
+  return (
+    <div className="login-form-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+        className="login-website-logo-mobile-img"
+        alt="website logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+        className="login-img"
+        alt="website login"
+      />
+      <form className="form-container" onSubmit={submitForm}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-desktop-img"
+          alt="website logo"
+        />
+        <div className="input-container">{renderUsernameField()}</div>
+        <div className="input-container">{renderPasswordField()}</div>
+        <button type="submit" className="login-button">
+          Login
+        </button>
+        {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
